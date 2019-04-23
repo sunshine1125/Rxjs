02cc07be4de7221ba782fadb54345f38d001a7f0
mafeifan <maf@shinetechchina.com>
2019-04-23 22:33:11 +0800

    add some create operators

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedRoute.url
+    .subscribe(url => console.log('The URL changed to: ' + url));
+  }
 }
